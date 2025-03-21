7610550f48515c9df7f189cf890cf62d7ded1776
Brian <brian.rodrigues@silabs.com>
2025-03-21 12:03:01 +0100

    Heap Monitor working, brought back Low Power

---
+++ heap_monitor_task.h
@@ -0,0 +1,15 @@
+
+#ifndef __HEAP_MONITOR_TASK_H
+#define __HEAP_MONITOR_TASK_H
+
+#include "sl_status.h"
+
+/**
+ * @brief Start the Heap Monitor task context.
+ *
+ * @return sl_status_t The status of the operation.
+ */
+sl_status_t start_heap_monitor_task_context(void);
+
+
+#endif // __HEAP_MONITOR_TASK_H
