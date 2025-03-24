716c9d44c19a4e1c6418166e1ec2f2da1d8d392f
Brian <brian.rodrigues@silabs.com>
2025-03-24 11:50:22 +0100

    Added Low Power (tickless) compat

--- heap_monitor_task_config.h
+++ heap_monitor_task_config.h
@@ -2,6 +2,7 @@
 #ifndef __HEAP_MONITOR_TASK_CONFIG_H
 #define __HEAP_MONITOR_TASK_CONFIG_H
 
+#define HEAP_MONITOR_TICKLESS           1
 #define HEAP_MONITOR_CHECK_PERIOD_MS    5000
 
 #define HEAP_MONITOR_RREDZONE_PERCENT   10 // Defines the amount of remaining HEAP at which point we print a warning Message
